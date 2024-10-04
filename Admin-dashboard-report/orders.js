bd85c5bf2674c037c982e990a7531c67d5fe2f01
Vo Xuan Duong <xuanduong.261104@gmail.com>
2024-10-04 22:16:39 +0700

    add new admin-dashboard-report

---
+++ Admin-dashboard-report/orders.js
@@ -0,0 +1,24 @@
+const Orders = [
+    {
+        ID: '11111',
+        tenKhachHang: '85743',
+        dichVu: 'cắt tóc',
+        thanhToan: 'Due',
+        ngayGanNhat: '11/12/2024'
+        
+    },
+    {
+        ID: '11111',
+        tenKhachHang: '97245',
+        dichVu: 'cắt tóc',
+        thanhToan: 'Refunded',
+        ngayGanNhat: '11/12/2024'
+    },
+    {
+        ID: '11111',
+        tenKhachHang: '36452',
+        dichVu: 'cắt tóc',
+        thanhToan: 'Paid',
+        ngayGanNhat: '11/12/2024'
+    },
+]
